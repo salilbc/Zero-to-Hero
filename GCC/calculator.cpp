c98a5e61bbf244f1a1cd6ce135f8b857fb45e6c4
Salil Cuncoliencar <Salil_Cuncoliencar@intuit.com>
2019-10-02 13:39:53 +0530

    Simple calculator program

---
+++ GCC/calculator.cpp
@@ -0,0 +1,32 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    char op;
+    float num1, num2;
+    cout << "Enter two mumbers: ";
+    cin >> num1 >> num2;
+    cout << "Enter operation to perform [+, -, *, /] : ";
+    cin >> op;
+    switch(op)
+    {
+        case '+':
+            cout << num1+num2;
+            break;
+        case '-':
+            cout << num1-num2;
+            break;
+        case '*':
+            cout << num1*num2;
+            break;
+        case '/':
+            cout << num1/num2;
+            break;
+        default:
+            // If the operator is other than +, -, * or /, error message is shown
+            cout << "Error! operator is not correct";
+            break;
+    }
+    return 0;
+}
