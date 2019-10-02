a538f72e84385addbc4a56aa2bb9f5d70e3e5edf
Allen Joseph <allenxavier45@gmail.com>
2019-10-02 17:01:52 +0530

    // Added Turbo c++ program (#24)

---
+++ Turbo C++/Sum_of_two_numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+#include <conio.h>
+
+void main()
+{
+  int a, b, c;
+  cout << "Enter two integers to add\n";
+  cin >> a >> b;
+  c = a + b;
+
+  cout <<"Sum of the numbers: " << c << endl;
+  return 0;
+}
